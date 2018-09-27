720b7a1f7dd274c6b460724836709dddc36f90d1
ttowncompiled <ian.riley.915@gmail.com>
2018-09-27 08:34:40 -0500

    Conforms solution to be more C-like/Rust-like.

---
+++ Greetings/julia/parallel_greeting.jl
@@ -0,0 +1,33 @@
+using Distributed
+@everywhere using Printf
+
+@everywhere function Greet(comm_sz, my_rank)
+    @sprintf "Greetings from process %d of %d" my_rank comm_sz
+end
+
+@everywhere function Greet!(comm, comm_sz, my_rank)
+    greeting = Greet(comm_sz, my_rank)
+    put!(comm, greeting)
+end
+
+function main()
+    comm_sz = length(workers())+1
+    comm_world = RemoteChannel(()->Channel{String}(comm_sz))
+
+    my_rank = 1
+
+    for rank in workers()
+        remote_do(Greet!, rank, comm_world, comm_sz, rank)
+    end
+
+    greeting = Greet(comm_sz, my_rank)
+    @printf "%s\n" greeting
+
+    for p in 2:comm_sz
+        greeting = take!(comm_world)
+        @printf "%s\n" greeting
+    end
+end
+
+main()
+
