8afce3c89c93a08d4cd94c3924326cded640ef88
ttowncompiled <ian.riley.915@gmail.com>
2018-09-27 18:04:35 -0500

    Conforms C and Rust solutions to be alike.

--- DotProduct/c-mpi/mpi_dot_product.c
+++ DotProduct/c-mpi/mpi_dot_product.c
@@ -1,37 +1,47 @@
-#include <stdio.h>      /* for printf               */
-#include <stdlib.h>     /* for malloc               */
-#include <mpi.h>        /* for mpi functions, etc   */
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
 
-const int n = 1024;     /* The size of each vector  */
-const double k = 2.0;   /* The size of the scalar   */
+const int N = 1024;
+const double K = 2.0;
+
+void DotProduct(int n,      /* in */
+                double k,   /* in */
+                double *x,  /* in */
+                double *y,  /* in */
+                double *z   /* out */) {
+    for (int i = 0; i < n; i++) {
+        *(z+i) = ( *(x+i) + *(y+i) ) * k;
+    }
+}
 
 int main(void) {
-    double      *x;         /* The first vector x           */
-    double      *y;         /* The second vector y          */
-    double      *z;         /* The output vector z          */
-    double      local_n;    /* The local block size of n    */
-    double      *local_x;   /* The local block of x         */
-    double      *local_y;   /* The local block of y         */
-    double      *local_z;   /* The local block of z         */
-    int         comm_sz;    /* number of processes          */
-    int         my_rank;    /* my process rank              */
+    double      x[N];
+    double      y[N];
+    double      z[N];
+
+    double      local_n;
+    double      *local_x;
+    double      *local_y;
+    double      *local_z;
+
+    int         comm_sz;
+    int         my_rank;
 
     MPI_Init(NULL, NULL);
     MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
     if (my_rank == 0) {
-        x = malloc(sizeof(double) * n);
-        y = malloc(sizeof(double) * n);
-        for (int i = 0; i < n; i++) {
-            *(x+i) = i+1;
+        for (int i = 0; i < N; i++) {
+            x[i] = i+1;
         }
-        for (int j = 0; j < n; j++) {
-            *(y+j) = j+1;
+        for (int j = 0; j < N; j++) {
+            y[j] = j+1;
         }
     }
 
-    local_n = n/comm_sz;
+    local_n = N/comm_sz;
 
     local_x = malloc(sizeof(double) * local_n);
     local_y = malloc(sizeof(double) * local_n);
@@ -42,20 +52,14 @@ int main(void) {
     MPI_Scatter(y, local_n, MPI_DOUBLE,
             local_y, local_n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-    for (int m = 0; m < local_n; m++) {
-        *(local_z+m) = ( *(local_x+m) + *(local_y+m) )*k;
-    }
-
-    if (my_rank == 0) {
-        z = malloc(sizeof(double) * n);
-    }
+    DotProduct(local_n, K, local_x, local_y, local_z);
 
     MPI_Gather(local_z, local_n, MPI_DOUBLE,
             z, local_n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
     if (my_rank == 0) {
-        for (int p = 0; p < n; p++) {
-            printf("%.2lf ", *(z+p));
+        for (int m = 0; m < N; m++) {
+            printf("%.2lf ", z[m]);
         }
         printf("\n");
     }
--- DotProduct/rust/concurrent_dot_product.rs
+++ DotProduct/rust/concurrent_dot_product.rs
@@ -6,34 +6,46 @@ const N: usize = 1024;
 const K: f64 = 2.0;
 
 #[allow(non_snake_case)]
-fn DotProduct(local_n: usize,       /* in */
-              k: f64,               /* in */
-              x: &[f64],            /* in */
-              y: &[f64],            /* in */
-              local_z: &mut[f64]    /* out */) {
-    for i in 0..local_n {
-        local_z[i] = (x[i] + y[i]) * k;
+fn DotProduct(n: usize,     /* in */
+              k: f64,       /* in */
+              x: &[f64],    /* in */
+              y: &[f64],    /* in */
+              z: &mut[f64]  /* out */) {
+    for i in 0..n {
+        z[i] = (x[i] + y[i]) * k;
     }
 }
 
 fn main() {
+    let mut x: [f64; N];
+    let mut y: [f64; N];
+    let mut z: [f64; N];
+
+    let local_n: usize;
+    let _local_x: &[f64];
+    let _local_y: &[f64];
+    let mut local_z: Vec<f64>;
+
+    let comm_sz: i32;
+    let _my_rank: i32;
+
     let args: Vec<String> = env::args().collect();
 
-    let comm_sz = *(&args[2].parse::<i32>().unwrap());
-    let _my_rank = 0;
-
+    comm_sz = *(&args[2].parse::<i32>().unwrap());
+    _my_rank = 0;
     let (comm_world_tx, comm_world_rx) = mpsc::channel();
 
-    let mut x = [0.0; N];
+    x = [0.0; N];
     for i in 0..N {
         x[i] = (i as f64) + 1.0;
     }
-    let mut y = [0.0; N];
+    y = [0.0; N];
     for j in 0..N {
         y[j] = (j as f64) + 1.0;
     }
+    z = [0.0; N];
 
-    let local_n = N/(comm_sz as usize);
+    local_n = N/(comm_sz as usize);
 
     for rank in 1..comm_sz {
         let comm_world_tx_clone = mpsc::Sender::clone(&comm_world_tx);
@@ -42,15 +54,15 @@ fn main() {
             let local_b = (local_a as usize) + local_n;
             let local_x = &x[local_a..local_b];
             let local_y = &y[local_a..local_b];
+
             let mut local_z = vec![0.0; local_n];
             DotProduct(local_n, K, local_x, local_y, &mut local_z);
+
             comm_world_tx_clone.send((rank, local_z)).unwrap();
         });
     }
 
-    let mut z = [0.0; N];
-
-    let mut local_z = vec![0.0; local_n];
+    local_z = vec![0.0; local_n];
     DotProduct(local_n, K, &x[0..local_n], &y[0..local_n], &mut local_z);
     for m in 0..local_n {
         z[m] = local_z[m];
