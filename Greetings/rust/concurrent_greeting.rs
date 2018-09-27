a469d70a4627268227c4361cd09a91d17443ac23
ttowncompiled <ian.riley.915@gmail.com>
2018-09-27 08:28:57 -0500

    Adds Rust solution to the Greetings parallel program.

---
+++ Greetings/rust/concurrent_greeting.rs
@@ -0,0 +1,34 @@
+use std::env;
+use std::sync::mpsc;
+use std::thread;
+
+#[allow(non_snake_case)]
+fn Greet(comm_sz: i32, my_rank: i32) -> String {
+    format!("Greetings from process {} of {}!", my_rank, comm_sz)
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let comm_sz = *(&args[2].parse::<i32>().unwrap());
+    let my_rank = 0;
+
+    let (tx, rx) = mpsc::channel();
+
+    for rank in 1..comm_sz {
+        let tx_clone = mpsc::Sender::clone(&tx);
+        thread::spawn(move || {
+            let greeting = Greet(comm_sz, rank);
+            tx_clone.send(greeting).unwrap();
+        });
+    }
+
+    let greeting = Greet(comm_sz, my_rank);
+    println!("{}", greeting);
+
+    for _ in 1..comm_sz {
+        let greeting = rx.recv().unwrap();
+        println!("{}", greeting);
+    }
+}
+
