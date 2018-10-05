4b83fa176fed3331468e5a8dbca06cee6eb6c351
ttowncompiled <ian.riley.915@gmail.com>
2018-10-05 08:46:02 -0500

    Experiment work with MPI_Reduce for class assignment.

--- Trapezoidal/c-mpi/mpi_trapezoidal.c
+++ Trapezoidal/c-mpi/mpi_trapezoidal.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <mpi.h>
 
-const int N = 1024;
+const int N = 1048576;
 const double A = 0.0;
-const double B = 100.0;
+const double B = 1000.0;
 
 double f(double x) {
     return 10*x;
@@ -27,28 +27,37 @@ double Trap(int comm_sz, int my_rank, int n, double a, double b) {
 }
 
 int main(void) {
-    double local_int;
-    double total_int;
+    double      local_int;
+    double      total_int;
 
     int         comm_sz;
     int         my_rank;
+
+    double      local_start;
+    double      local_finish;
+    double      local_elapsed;
+    double      elapsed;
 
     MPI_Init(NULL, NULL);
     MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
-    if (my_rank != 0) {
-        local_int = Trap(comm_sz, my_rank, N, A, B);
-        MPI_Send(&local_int, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
-    } else {
-        local_int = Trap(comm_sz, my_rank, N, A, B);
-        total_int = local_int;
-        for (int q = 1; q < comm_sz; q++) {
-            MPI_Recv(&local_int, 1, MPI_DOUBLE, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            total_int += local_int;
-        }
+    local_int = Trap(comm_sz, my_rank, N, A, B);
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    local_start = MPI_Wtime();
+
+    MPI_Reduce(&local_int, &total_int, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    local_finish = MPI_Wtime();
+    local_elapsed = local_finish - local_start;
+
+    MPI_Reduce(&local_elapsed, &elapsed, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+
+    if (my_rank == 0) {
         printf("With n = %d trapezoids, our estimate\n", N);
         printf("of the integral from %.2f to %.2f = %.15e\n", A, B, total_int);
+        printf("Elapsed time = %e seconds\n", elapsed);
     }
 
     MPI_Finalize();
