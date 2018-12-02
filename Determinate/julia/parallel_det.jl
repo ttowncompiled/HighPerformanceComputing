e63e837538a2f1b431ab648cf04bc7d311b09453
ttowncompiled <ian.riley.915@gmail.com>
2018-12-02 17:08:13 -0600

    Adds distributed solution for julia det.

--- Determinate/julia/parallel_det.jl
+++ Determinate/julia/parallel_det.jl
@@ -1,20 +1,18 @@
 using Distributed
 @everywhere using Printf
-
-@everywhere const FLT_MAX = 3.4028234664e+38
+using Random
 
 const N = 256
-const A_MAX = 10
 const SEED = 256
 
 @everywhere function Determinate(a, n)
     d = 1.0
     for i in 1:n
-        d = (d * (a[i][i] % FLT_MAX)) % FLT_MAX
+        d = d * a[i, i]
         for j in (i+1):n
-            z = a[j][i] / a[i][i]
+            z = a[j, i] / a[i, i]
             for k in (i+1):n
-                a[j][k] = a[j][k] - z * a[i][k]
+                a[j, k] = a[j, k] - z * a[i, k]
             end
         end
     end
@@ -22,22 +20,22 @@ const SEED = 256
 end
 
 @everywhere function Compact(my_rank, comm_sz, a, n)
-    local_a = Array{Float64}(undef, n-1, n-1)
     local_det = 0.0
-    for k in my_rank:n:comm_sz
-        z = a[1][k]
-        for i in 2:N
-            for j in 1:K
-                local_a[i-1][j] = a[i][j]
+    local_a = Array{Float64}(undef, n-1, n-1)
+    for k in my_rank:comm_sz:n
+        z = a[1, k]
+        for i in 2:n
+            for j in 1:(k-1)
+                local_a[i-1, j] = a[i, j]
             end
         end
-        for i in (k+1):n
+        for i in 2:n
             for j in (k+1):n
-                local_a[i-1][j-1] = a[i][j]
+                local_a[i-1, j-1] = a[i, j]
             end
         end
         d = Determinate(local_a, n-1)
-        local_det = (local_det + (z * d * (if (k % 2 == 1) 1 else -1 end)) % FLT_MAX) % FLT_MAX
+        local_det = local_det + (z * d * if (k % 2 == 1) 1 else -1 end)
     end
     local_det
 end
@@ -50,22 +48,24 @@ end
 function main()
     comm_sz = length(workers())+1
     my_rank = 1
-    comm_world = RemoteChannel(()->Channel{Float64}(comm_sz))
+    comm_world = RemoteChannel(()->Channel{Float64}(N))
 
-    srand(SEED)
-    a = rand(1:A_MAX, N, N)
+    Random.seed!(SEED)
+    a = rand(N, N)
 
-    for rank in wokers()
+    for rank in workers()
         remote_do(Work!, rank, comm_world, rank, comm_sz, a, N)
     end
 
-    det = 0
+    det = Compact(my_rank, comm_sz, a, N)
     for _ in 2:comm_sz
         local_det = take!(comm_world)
-        det = (det + local_det) % FLT_MAX
+        det = det + local_det
     end
 
     det = abs(det)
     @printf "Determinate = %e\n" det
 
 end
+
+main()
