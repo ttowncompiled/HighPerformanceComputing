81f2842796e76d013e193058f70b580994779e1f
ttowncompiled <ian.riley.915@gmail.com>
2018-12-03 08:19:34 -0600

    Adds concurrent solution for determinant in rust.

--- Determinant/rust/concurrent_det/src/main.rs
+++ Determinant/rust/concurrent_det/src/main.rs
@@ -2,6 +2,9 @@ extern crate rand;
 
 use rand::prelude::*;
 use rand::rngs::StdRng;
+use std::sync::Arc;
+use std::thread;
+use std::thread::JoinHandle;
 use std::time::SystemTime;
 
 const N: usize = 256;
@@ -10,23 +13,48 @@ const SEED: u64 = 256;
 fn main() {
     let mut rng = StdRng::seed_from_u64(SEED);
 
-    let mut a: [f64; N*N] = [0.0; N*N];
-    for i in 0..N {
-        for j in 0..N {
-            a[i*N + j] = rng.gen();
-        }
+    let mut a: Vec<Arc<Vec<f64>>> = Vec::new();
+    for _ in 0..N {
+        a.push(Arc::new(
+            (0..N).map(|_| {
+                rng.gen()
+            }).collect()
+        ));
     }
 
     let start = SystemTime::now();
 
+    let mut handles: Vec<JoinHandle<_>> = Vec::new();
+
     let mut d: f64 = 1.0;
     for i in 0..N {
-        d = d * a[i*N + i];
+        d = d * a[i][i];
         for j in (i+1)..N {
-            let z: f64 = a[j*N + i] / a[i*N + i];
-            for k in (i+1)..N {
-                let val = a[j*N + k] - z * a[i*N + k];
-                a[j*N + k] = val;
+            let a_i = a[i].clone();
+            let a_j = a[j].clone();
+            let handle: JoinHandle<_> = thread::spawn(move || {
+                match Arc::get_mut(&mut a_j.clone()) {
+                    Some(ref mut a_j) => {
+                        let z: f64 = a_j[i] / a_i[i];
+                        for k in (i+1)..N {
+                            let val = a_j[k] - z * a_i[k];
+                            a_j[k] = val;
+                        }
+                    },
+                    None => (),
+                }
+            });
+            handles.push(handle);
+        }
+        while ! handles.is_empty() {
+            match handles.pop() {
+                Some(handle) => {
+                    match handle.join() {
+                        Ok(_) => (),
+                        Err(e) => println!("Error: {:?}", e),
+                    }
+                },
+                None => (),
             }
         }
     }
