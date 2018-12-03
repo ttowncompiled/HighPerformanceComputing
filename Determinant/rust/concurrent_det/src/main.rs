e033ddc4f2e6f1dc12dbaa7181cc2ad920724208
ttowncompiled <ian.riley.915@gmail.com>
2018-12-03 07:51:19 -0600

    Times rust solution to determinant.

--- Determinant/rust/concurrent_det/src/main.rs
+++ Determinant/rust/concurrent_det/src/main.rs
@@ -2,11 +2,10 @@ extern crate rand;
 
 use rand::prelude::*;
 use rand::rngs::StdRng;
+use std::time::SystemTime;
 
-const N: usize = 3;
-const A_MAX: i32 = 10;
+const N: usize = 256;
 const SEED: u64 = 256;
-const FLT_MAX: f64 = 3.402823e+38;
 
 fn main() {
     let mut rng = StdRng::seed_from_u64(SEED);
@@ -14,13 +13,15 @@ fn main() {
     let mut a: [f64; N*N] = [0.0; N*N];
     for i in 0..N {
         for j in 0..N {
-            a[i*N + j] = (rng.gen_range(0, A_MAX)+1).into();
+            a[i*N + j] = rng.gen();
         }
     }
 
+    let start = SystemTime::now();
+
     let mut d: f64 = 1.0;
     for i in 0..N {
-        d = (d * (a[i*N + i] % FLT_MAX)) % FLT_MAX;
+        d = d * a[i*N + i];
         for j in (i+1)..N {
             let z: f64 = a[j*N + i] / a[i*N + i];
             for k in (i+1)..N {
@@ -31,4 +32,11 @@ fn main() {
     }
 
     println!("Determinant = {:e}", d.abs());
+    match start.elapsed() {
+        Ok(elapsed) => {
+            let s = elapsed.as_secs() as f64 + elapsed.subsec_nanos() as f64 * 1e-9;
+            println!("Elapsed time = {:e} seconds", s);
+        },
+        Err(e) => println!("Error: {:?}", e),
+    }
 }
