b9a9d4a3ae1e8d088c498c3dec4a4af4f3870941
ttowncompiled <ian.riley.915@gmail.com>
2018-12-05 15:17:35 -0600

    Adds SLURM run scripts.

---
+++ Determinant/c-mpi/run_64_512.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+#SBATCH --job-name=mpi_det.c
+#SBATCH --error=log/job.%J.err
+#SBATCH --output=log/job.%J.out
+#SBATCH --ntasks=64
+#SBATCH --ntasks-per-node=16
+#SBATCH --ntasks-per-core=1
+#SBATCH --time=0
+#SBATCH --mem-per-cpu=100
+
+module load gcc
+module load openmpi
+
+cd ~/HighPerformanceComputing/Determinant/c-mpi/
+mpirun ./mpi_det.exe 512 256
