205a7327cc4fa319f71d4c3a4be37904761c1d0d
ttowncompiled <ian.riley.915@gmail.com>
2018-12-07 07:40:44 -0600

    Refactors critical section out of thread computation.

--- N-BodySolver/c/n_body_solver.c
+++ N-BodySolver/c/n_body_solver.c
@@ -10,20 +10,23 @@ const double G = 6.673e-11;
 struct arg_struct {
     int q;
     int n;
-    double* masses;
-    double** s;
-    double** v;
+    double *masses;
+    double **s;
+    double **f;
 };
 
 void* ThreadWork(void* args_) {
     struct arg_struct *args = (struct arg_struct*) args_;
     int q = args->q;
     int n = args->n;
-    double* masses = args->masses;
+    double *masses = args->masses;
     double **s = args->s;
-    double **v = args->v;
+    double **f = args->f;
 
-    double force_q[DIMS];
+    for (int d = 0; d < DIMS; d++) {
+        f[q][d] = 0.0;
+    }
+
     for (int k = 0; k < n; k++) {
         if (k == q) {
             continue;
@@ -37,19 +40,15 @@ void* ThreadWork(void* args_) {
         dist = sqrt(dist);
         double dist_cubed = dist*dist*dist;
         for (int d = 0; d < DIMS; d++) {
-            force_q[d] += G*masses[q]*masses[k]/dist_cubed * diff[d];
+            f[q][d] += G*masses[q]*masses[k]/dist_cubed * diff[d];
         }
-    }
-    for (int d = 0; d < DIMS; d++) {
-        s[q][d] += v[q][d];
-        v[q][d] += force_q[d] / masses[q];
     }
 
     free(args);
     return NULL;
 }
 
-void Do(long n, double *masses, double **s, double **v, long steps) {
+void Do(long n, double *masses, double **s, double **v, double **f, long steps) {
     pthread_t* thread_handles = (pthread_t*) malloc(n*sizeof(pthread_t));
     for (int t = 0; t <= steps; t++) {
         struct arg_struct *args;
@@ -59,11 +58,17 @@ void Do(long n, double *masses, double **s, double **v, long steps) {
             args->n = n;
             args->masses = masses;
             args->s = s;
-            args->v = v;
+            args->f = f;
             pthread_create(&thread_handles[q], NULL, ThreadWork, (void*) args);
         }
         for (int q = 0; q < n; q++) {
             pthread_join(thread_handles[q], NULL);
+        }
+        for (int q = 0; q < n; q++) {
+            for (int d = 0; d < DIMS; d++) {
+                s[q][d] += v[q][d];
+                v[q][d] += f[q][d] / masses[q];
+            }
         }
     }
     for (int q = 0; q < n; q++) {
@@ -89,11 +94,13 @@ int main(int argc, char* argv[]) {
     double *masses = (double*) malloc(n*sizeof(double));
     double **s = (double**) malloc(n*sizeof(double*));
     double **v = (double**) malloc(n*sizeof(double*));
+    double **f = (double**) malloc(n*sizeof(double*));
 
     for (int q = 0; q < n; q++) {
         masses[q] = mass;
         s[q] = (double*) malloc(DIMS*sizeof(double));
         v[q] = (double*) malloc(DIMS*sizeof(double));
+        f[q] = (double*) malloc(DIMS*sizeof(double));
         s[q][0] = q * gap;
         for (int d = 1; d < DIMS; d++) {
             s[q][d] = 0.0;
@@ -106,11 +113,14 @@ int main(int argc, char* argv[]) {
                 v[q][d] = -speed;
             }
         }
+        for (int d = 0; d < DIMS; d++) {
+            f[q][d] = 0.0;
+        }
     }
 
     clock_t start = clock();
 
-    Do(n, masses, s, v, steps);
+    Do(n, masses, s, v, f, steps);
 
     clock_t finish = clock();
     printf("Elapsed time = %e seconds\n", (double) (finish - start) / CLOCKS_PER_SEC);
