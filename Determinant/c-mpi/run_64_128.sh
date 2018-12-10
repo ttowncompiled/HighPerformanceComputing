c5e3be024e703bb4a0a5ecfc8096859c62768040
ttowncompiled <ian.riley.915@gmail.com>
2018-12-10 14:55:23 -0600

    Refactors run scripts.

--- Determinant/c-mpi/run_64_128.sh
+++ Determinant/c-mpi/run_64_128.sh
@@ -3,8 +3,8 @@
 #SBATCH --job-name=mpi_det.c
 #SBATCH --error=log/job.%J.err
 #SBATCH --output=log/job.%J.out
-#SBATCH --ntasks=8
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks=64
+#SBATCH --ntasks-per-node=8
 #SBATCH --ntasks-per-core=1
 #SBATCH --time=0
 #SBATCH --mem-per-cpu=100
