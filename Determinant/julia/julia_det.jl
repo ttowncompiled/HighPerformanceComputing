6728b357d974a1c3efb91a1852dd5b1f375d1858
ttowncompiled <ian.riley.915@gmail.com>
2018-12-11 13:11:32 -0600

    Refactors julia to use MPI.

--- Determinant/julia/julia_det.jl
+++ Determinant/julia/julia_det.jl
@@ -1,8 +1,8 @@
-using Distributed
-@everywhere using Printf
+import MPI
+using Printf
 using Random
 
-@everywhere function CalculateLogDeterminantOf(n, a)
+function CalculateLogDeterminantOf(n, a)
     sign_d::Int64 = 1
     log_d::Float64 = 0.0
 
@@ -27,7 +27,7 @@ using Random
     sign_d, log_d + log2(abs(d))
 end
 
-@everywhere function CalculateLogDeterminantOfAfterCompacting(my_rank, comm_sz, n, a)
+function CalculateLogDeterminantOfAfterCompacting(my_rank, comm_sz, n, a)
     local_a::Array{Float64, 2} = Array{Float64}(undef, n-1, n-1)
 
     log_x_0::Float64 = -1.0
@@ -36,16 +36,16 @@ end
     sum_x::Float64 = 0.0
     sum_y::Float64 = 0.0
 
-    for k in my_rank:comm_sz:n
-        c_k::Float64 = a[1, k]
+    for k in (my_rank+1):comm_sz:n
+        c_k::Float64 = a[k]
         for i in 2:n
             for j in 1:(k-1)
-                local_a[i-1, j] = a[i, j]
+                local_a[i-1, j] = a[(i-1)*n + j]
             end
         end
         for i in 2:n
             for j in (k+1):n
-                local_a[i-1, j-1] = a[i, j]
+                local_a[i-1, j-1] = a[(i-1)*n + j]
             end
         end
 
@@ -83,14 +83,13 @@ end
     end
 end
 
-@everywhere function RemoteCalculateLogDeterminantOfAfterCompacting(comm, my_rank, comm_sz, n, a)
-    sign_det::Int64, local_det::Float64 = CalculateLogDeterminantOfAfterCompacting(my_rank, comm_sz, n, a)
-    put!(comm, (sign_det, local_det))
-end
-
-function Do(comm_world, my_rank, comm_sz, n, a)
-    for rank in workers()
-        remote_do(RemoteCalculateLogDeterminantOfAfterCompacting, rank, comm_world, rank, comm_sz, n, a)
+function Do(my_rank, comm_sz, n, a)
+    if my_rank == 0
+        for rank in 1:(comm_sz-1)
+            MPI.Send(a, rank, 0, MPI.COMM_WORLD)
+        end
+    else
+        MPI.Recv!(a, 0, 0, MPI.COMM_WORLD)
     end
 
     log_x_0::Float64 = -1.0
@@ -107,50 +106,72 @@ function Do(comm_world, my_rank, comm_sz, n, a)
         log_y_0 = local_det
     end
 
-    for _ in 2:comm_sz
-        sign_det, local_det = take!(comm_world)
-        if sign_det > 0
-            if log_x_0 < 0
-                log_x_0 = local_det
+    local_pair::Array{Float64} = Array{Float64}(undef, 2)
+
+    if my_rank == 0
+        for rank in 1:(comm_sz-1)
+            MPI.Recv!(local_pair, rank, 0, MPI.COMM_WORLD)
+            if local_pair[1] > 0
+                if log_x_0 < 0
+                    log_x_0 = local_pair[2]
+                else
+                    sum_x += 2^(local_pair[2] - log_x_0)
+                end
             else
-                sum_x += 2^(local_det - log_x_0)
-            end
-        else
-            if log_y_0 < 0
-                log_y_0 = local_det
-            else
-                sum_y += 2^(local_det - log_y_0)
+                if log_y_0 < 0
+                    log_y_0 = local_pair[2]
+                else
+                    sum_y += 2^(local_pair[2] - log_y_0)
+                end
             end
         end
-    end
 
-    log_x::Float64 = log_x_0 + log2(1 + sum_x)
-    log_y::Float64 = log_y_0 + log2(1 + sum_y)
+        log_x::Float64 = log_x_0 + log2(1 + sum_x)
+        log_y::Float64 = log_y_0 + log2(1 + sum_y)
 
-    det::Float64 = if log_x > log_y
-        log_x + log2(1 - 2^(log_y - log_x))
+        det::Float64 = if log_x > log_y
+                log_x + log2(1 - 2^(log_y - log_x))
+            else
+                log_y + log2(1 - 2^(log_x - log_y))
+            end
+
+        det = det / log2(exp(1))
+        @printf "log(abs(det)) = %e\n" det
     else
-        log_y + log2(1 - 2^(log_x - log_y))
+        local_pair[1] = convert(Float64, sign_det)
+        local_pair[2] = local_det
+        MPI.Send(local_pair, 0, 0, MPI.COMM_WORLD)
     end
-
-    det = det / log2(exp(1))
-    @printf "log(abs(det)) = %e\n" det
 end
 
 function main()
     n::Int64 = parse(Int64, ARGS[1])
     seed::Int64 = parse(Int64, ARGS[2])
 
-    comm_sz::Int64 = length(workers())+1
-    my_rank::Int64 = 1
-    comm_world::RemoteChannel{Channel{Tuple{Int64, Float64}}} = RemoteChannel(()->Channel{Tuple{Int64, Float64}}(comm_sz))
+    MPI.Init()
+    comm_sz::Int64 = MPI.Comm_size(MPI.COMM_WORLD)
+    my_rank::Int64 = MPI.Comm_rank(MPI.COMM_WORLD)
 
     @printf "my_rank=%d, comm_sz=%d, n=%d, seed=%d\n" my_rank comm_sz n seed
 
-    Random.seed!(seed)
-    a::Array{Float64, 2} = rand(n, n) .- 0.5
+    a::Array{Float64} = if my_rank == 0
+            Random.seed!(seed)
+            rand(n*n) .- 0.5
+        else
+            Array{Float64}(undef, n*n)
+        end
 
-    @time Do(comm_world, my_rank, comm_sz, n, a)
+    MPI.Barrier(MPI.COMM_WORLD)
+
+    if my_rank == 0
+        @time Do(my_rank, comm_sz, n, a)
+    else
+        Do(my_rank, comm_sz, n, a)
+    end
+
+    MPI.Barrier(MPI.COMM_WORLD)
+
+    MPI.Finalize()
 end
 
 main()
