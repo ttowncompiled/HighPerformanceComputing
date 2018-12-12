65ccbec2d3c283c053ac9b04720c9c0be2c9c53d
ttowncompiled <ian.riley.915@gmail.com>
2018-12-11 20:38:40 -0600

    Refactors run scripts.

---
+++ Determinant/c-mpi/run_32_512.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+parallel-scp -h ~/machinefile ~/HighPerformanceComputing/Determinant/c-mpi/mpi_det.exe ~/HighPerformanceComputing/Determinant/c-mpi/
+mpirun -np 32 --hostfile ~/machinefile ~/HighPerformanceComputing/Determinant/c-mpi/mpi_det.exe 512 1
